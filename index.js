78c8d41f8cdc81dbf069c8e3d75d6371c031b42f
Gavrosh Cyril <gavrosh.cyril@gmail.com>
2022-06-14 22:07:37 +0300

    fix: the design of the Projects and Code section

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ for (let anchor of anchors) {
     })
   })
 }
+
+
+const burgerMenu = document.querySelector('.burger-menu');
+const headerNav = document.querySelector('.header-nav');
+const closeBtn = document.querySelector('.close-btn');
+
+burgerMenu.addEventListener('click', () => {
+  headerNav.classList.add('active');
+  closeBtn.classList.remove('hide');
+});
+
+closeBtn.addEventListener('click', () => {
+    headerNav.classList.remove('active');
+    closeBtn.classList.add('hide');
+})
